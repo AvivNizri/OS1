802f581110c5328034ddbe90912dd89f9e59d17b
avivNizri <avivnizri70@gmail.com>
2019-12-03 11:36:42 -0800

    debuging

--- codeExecution.c
+++ codeExecution.c
@@ -1,5 +1,5 @@
 /*
-  OsCourse task1 part2 -> C program to iterate over a directory recursively, and execute compilation and more
+  OsCourse task1 part2 -> C program to iterate over a directory , execute inner program and check expected result
 */
 
 #include <stdio.h>
@@ -8,8 +8,9 @@
 #include <fcntl.h>
 #include <dirent.h>
 
+#define studentProg "main.exe"
 
-void exploreFilesRecursively(char* basePath, char* input, char* output, dirent* filesComper);
+//void exploreFilesRecursively(char* basePath, char* input, char* output, struct dirent* filesComper, FILE* resultFile);
 
 int main(int argc, char* argv[])
 {
@@ -18,7 +19,7 @@ int main(int argc, char* argv[])
         return 0;
     }
 
-    int configFIle = open(strcat(argv[2],argv[1]), O_RDONLY);
+    int configFIle = fopen(strcat(argv[2],argv[1]), O_RDONLY);
 
     if(configFIle == -1){
         printf("There was an error opening the files\n exiting...");
@@ -51,12 +52,16 @@ int main(int argc, char* argv[])
         }
     }
 
-    int resultCSV = open("result.csv", O_WRONLY | O_CREAT | O_TRUNC , 0666);
+    FILE* resultCSV = fopen("result.csv", 'w');
+    //if(resultCSV == -1){
+    //  printf("Error while opening result.csv file!\n");
+    //  exit(1);
+    //}
 
     exploreFilesRecursively(line1, line2, line3, fc, resultCSV);
 
     // Closing files and buffers
-    close(resultCSV);
+    fclose(resultCSV);
     close(configFIle);
     free(line1);
     free(line2);
@@ -71,7 +76,7 @@ int main(int argc, char* argv[])
  * considering path as base path.
  */
 
-void exploreFilesRecursively(char* basePath, char* input, char* output, dirent* filesComper, int resultFile)
+void exploreFilesRecursively(char* basePath, char* input, char* output, struct dirent* filesComper, FILE* resultFile)
 {
     char path[1000];
     // dirent is the struct which holds dirPath with tons on prop
@@ -89,17 +94,17 @@ void exploreFilesRecursively(char* basePath, char* input, char* output, dirent* 
         // Avoid getting into hidden and system dir
         if (strcmp(dp->d_name, ".") != 0 && strcmp(dp->d_name, "..") != 0)
         {
-            //printf("Folder - %s\n", dp->d_name);
+            printf("Folder - %s\n", dp->d_name);
 
             // Building the char* argv input argument for the executed program
             char* prog_args[2];
 
             pid_t pid = fork();
 
-            if (pid == 0) {
-                // execution result file
-                int result = open("result.txt",O_WRONLY | O_APPEND);
+            // execution result file
+            int result = fopen("result.txt", O_WRONLY | O_APPEND);
 
+            if (pid == 0) {
                 // here the newfd is the file descriptor of stdout (i.e. 1) 
                 // All the printf statements will be written in the file 
                 dup2(result, 1); 
@@ -110,7 +115,7 @@ void exploreFilesRecursively(char* basePath, char* input, char* output, dirent* 
                 // executing the inner program with the relevant arguments
                 // execv("/usr/bin/gcc", dp->d_name);  -- thats for compilation
                 // also remember that the child process as been terminated
-                execvp(dp->d_name, prog_args);
+                execvp(studentProg, prog_args);
             }else{
                 close(result);
                 free(prog_args);
@@ -134,16 +139,14 @@ void exploreFilesRecursively(char* basePath, char* input, char* output, dirent* 
                 wait(&ret);
                 free(args);
             }
+            fprintf(resultFile,"%s", dp->d_name);
+            fprintf(resultFile,"%s", ',');
             if(ret == 2){
                 // write 100 to the student in the file
-                resultFile.write(dp->d_name);
-                resultFile.write(",");
-                resultFile.write("100");
+                fprintf(resultFile,"%s", '100\n');
             }else{
                 // write 0 to the student in the file
-                resultFile.write(dp->d_name);
-                resultFile.write(",");
-                resultFile.write("0");
+                fprintf(resultFile,"%s", '0\n');
             }
         }
     }
